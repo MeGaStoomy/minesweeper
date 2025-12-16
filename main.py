e67e33ccbc013471d7f6697d096996d4d17167e1
Stoomy <116960134+MeGaStoomy@users.noreply.github.com>
2025-12-16 11:10:08 +0100

    Window close and placement
    automatically centers the tkinter window on openning, and closes it when program ends

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import random, os
+import random, os, atexit
 
 ####################
 
@@ -29,28 +29,23 @@ def load_textures(folder):
         return dictTextures
 textures = load_textures('images')
 
-
-def mousePress(event):
-    global mouseDown
-    mouseDown = True
-def mouseRelease(event):
-    global mouseDown
-    mouseDown = False
-
 current_widget = None
 def gameCellPress(event):
-    mousePress(event)
+    global current_widget
     event.widget.config(image=textures['type0'])
+    current_widget = event.widget
 def gameCellUnpressed(event):
     global current_widget
-    mouseRelease(event)
     event.widget.config(image=textures['closed'])
     if current_widget:
         current_widget.config(image=textures['closed'])
+    if event.type == '5':
+        print((event.widget.cell.x, event.widget.cell.y))
+    print(event.type)
 def gameCellMotion(event):
     global current_widget
     widget = event.widget.winfo_containing(event.x_root, event.y_root)
-    if current_widget != widget:
+    if (widget) and (widget.cell.cellType == 'gameCell') and (current_widget != widget):
         if current_widget:
             current_widget.event_generate("<<Leave>>")
         current_widget = widget
@@ -59,14 +54,15 @@ def gameCellMotion(event):
 
 
 
+
 class Jeu:
     def __init__(self):
-        self.createBorderCells('normal')
-        #self.createGameCells()
+        self.createBorderCells(input('Quelle difficulté ? (easy, normal, hard) : '))
+        self.createGameCells()
 
     def createGameCells(self):
         textures
-        self.listeGameCells = [[Cell('gameCell', x,y) for x in range(9)] for y in range(9)]
+        self.listeGameCells = [[Cell('gameCell', x,y) for x in range(self.width-2)] for y in range(self.height-6)]
         self.listeGameLabels = []
         for y in range(len(self.listeGameCells)):
             self.listeGameLabels.append([Label(main, image=textures['closed'], height=38, width=38, bd=0, highlightthickness=0) for _ in range(len(self.listeGameCells[0]))])
@@ -79,21 +75,22 @@ class Jeu:
                 label.bind('<<Leave>>', gameCellUnpressed)
                 label.cell = self.listeGameCells[y][x]
                 self.listeGameCells[y][x].label = label
-                label.place(x=x*38, y=y*38)
+                label.place(x=38+x*38, y=38*5+y*38)
                 
     def createBorderCells(self, mode):
         textures
         if mode == 'easy':
-            width = 9+2
-            height = 9+6
+            self.width = 9+2
+            self.height = 9+6
         elif mode == 'normal':
-            width = 16+2
-            height = 16+6
+            self.width = 16+2
+            self.height = 11+6
         else:
-            width = 30+2
-            height = 16+6
-        main.geometry(f'{38*width}x{38*height}')
-        self.listeBorderCells = [[Cell('borderCell', x, y) for x in range(width)] for y in range(height)]
+            self.width = 30+2
+            self.height = 11+6
+        main.geometry(f'{38*self.width}x{38*self.height}')
+        self.placeWindow()
+        self.listeBorderCells = [[Cell('borderCell', x, y) for x in range(self.width)] for y in range(self.height)]
         self.listeBorderLabels = []
         borderTemplate = [['corner_up_left',     'border_hor',     'corner_up_right'],
                           ['border_vert',         'blank',             'border_vert'],
@@ -101,29 +98,40 @@ class Jeu:
                           ['border_vert',         'blank',             'border_vert'],
                           ['corner_bottom_left', 'border_hor', 'corner_bottom_right']]
         template_y = 0
-        for y in range(height):
+        for y in range(self.height):
             self.listeBorderLabels.append([])
             if not(y == 0):
                 if 0 < y < 4:
                     template_y = 1
                 elif y == 4:
                     template_y = 2
-                elif 4 < y < height-1:
+                elif 4 < y < self.height-1:
                     template_y = 3
                 else:
                     template_y = 4
-            for x in range(width):
+            for x in range(self.width):
                 template_x = 0
-                if 0 < x < width-1:
+                if 0 < x < self.width-1:
                     template_x = 1
-                elif x == width-1:
+                elif x == self.width-1:
                     template_x = 2
                 label = Label(main, image=textures[borderTemplate[template_y][template_x]], height=38, width=38, bd=0, highlightthickness=0)
                 self.listeBorderLabels[y].append(label)
                 self.listeBorderCells[y][x].label = label
                 label.cell = self.listeBorderCells[y][x]
                 label.place(x=38*x, y=38*y)
-                
+
+    def placeWindow(self):
+        main.update_idletasks()  # Let Tk calculate window size
+        w = main.winfo_width()
+        h = main.winfo_height()
+        screen_w = main.winfo_screenwidth()
+        screen_h = main.winfo_screenheight() - 85
+        x = (screen_w // 2) - (w // 2)
+        y = (screen_h // 2) - (h // 2)
+        main.geometry(f"{w}x{h}+{x}+{y}")
+
+    
                 
             
 class Cell:
@@ -134,3 +142,4 @@ class Cell:
         self.label = None
 
 jeuActuel = Jeu()
+atexit.register(main.destroy)
